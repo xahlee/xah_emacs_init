f0500fd3a9c13b3f265fb5f1b84baac516562c3f
Xah Lee <xah@xahlee.org>
2013-02-05 17:17:02 -0800

    routine update

--- xah_emacs_abbr.el
+++ xah_emacs_abbr.el
@@ -145,7 +145,7 @@
 
     ("8tla" "<div class=\"¤tla\"><a href=\"url\">text</a></div>")
     ("8ln" "(☛ )")
-    ("8menu" "〖a▸b▸c▸〗")
+    ("8menu" "〖a ▸ b ▸ c〗")
     ("8key" "【Alt+f】")
 
     ("8line" "────────── ────────── ────────── ────────── ──────────")
@@ -210,6 +210,8 @@
 ;;; unix shell
 
     ("3rsn" "rsync -z -r -v -t --chmod=Dugo+x --chmod=ugo+r --delete --exclude='*~' --exclude='.bash_history' --exclude='logs/'  --rsh='ssh -l u40651120' ~/web/ u40651120@s168753655.onlinehome.us:~/")
+    ("3rsn2" "rsync -z -r -v -t  --delete  ~/web/  /media/OS/Users/h3/web/")
+
     ("3ssh" "ssh -l u40651120 xahlee.org ")
 
     ("3ditto" "ditto -ck --sequesterRsrc --keepParent src dest")
