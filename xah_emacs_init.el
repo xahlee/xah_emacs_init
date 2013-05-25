0c54dce9efb806f64f6b854c3a47b494daf1498e
Xah Lee <xah@xahlee.org>
2013-05-25 12:31:08 -0700

    mod painful path exec-path

--- xah_emacs_init.el
+++ xah_emacs_init.el
@@ -49,25 +49,21 @@ interactively by “eval-buffer”."
   (let (
         (myPathList
          [
-           "C:/Python27"
-           "C:/Users/h3/apps/php-5.4.6-nts-Win32-VC9-x86/"
-           ;; "C:/Python32"
-           "C:/strawberry/c/bin"
-           "C:/strawberry/perl/site/bin"
-           "C:/strawberry/perl/bin"
-
-           "C:/Users/h3/AppData/Roaming/npm"
-           "C:/Program Files (x86)/nodejs/"
-
-           "C:/cygwin/usr/local/bin"
-           "C:/cygwin/usr/bin"
-           "C:/cygwin/bin"
-
-           "C:/Program Files (x86)/ErgoEmacs/msys/bin"
-           "C:/Program Files (x86)/Mozilla Firefox/"
-           "C:/Program Files (x86)/Opera"
-           "C:/Program Files (x86)/Safari"
-           "C:/Users/h3/AppData/Local/Google/Chrome/Application"
+"c:/cygwin/usr/local/bin"
+"C:/cygwin/bin"
+"C:/Program Files/Common Files/Microsoft Shared/Windows Live"
+"C:/Program Files (x86)/Common Files/Microsoft Shared/Windows Live"
+"C:/Windows/system32"
+"C:/Windows"
+"C:/Windows/System32/Wbem"
+"C:/Windows/System32/WindowsPowerShell/v1.0"
+"C:/Program Files (x86)/ATI Technologies/ATI.ACE/Core-Static"
+"C:/Program Files (x86)/Common Files/Roxio Shared/DLLShared"
+"C:/Program Files (x86)/Common Files/Roxio Shared/12.0/DLLShared"
+"C:/Program Files (x86)/Windows Live/Shared"
+"c:/Users/xah/apps/emacs-24.3/bin"
+"c:/Users/xah/apps/emacs-24.3/lib-src/oo-spd/i386"
+"c:/Users/xah/apps/emacs-24.3/lib-src/oo/i386"
            ] )
         )
 
@@ -75,6 +71,16 @@ interactively by “eval-buffer”."
 
     (setq exec-path (append myPathList (list "." exec-directory)) )
     ) )
+
+;(setenv "SHELL" "C:/Windows/system32/cmd.exe" )
+;(setenv "SHELL" "C:/Windows/system32/cmdproxy.exe" )
+
+;; Warning! shell-command-switch is "-c".
+;; You should set this to "/c" when using a system shell.
+
+;; Warning! w32-quote-process-args is t.
+;; You should set this to nil when using a system shell.
+
 
 
 
