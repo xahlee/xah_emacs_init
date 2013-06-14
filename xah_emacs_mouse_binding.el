6d197279967e640634fd2e02057c84aae235c51b
Xah Lee <xah@xahlee.org>
2013-06-14 03:42:44 -0700

    2013-06-14 update mouse binding for Microsoft Windows

--- xah_emacs_mouse_binding.el
+++ xah_emacs_mouse_binding.el
@@ -9,12 +9,24 @@
 ;; emacs mouse numbering changes depending on {OS, mouse, driver}.
 ;; http://xahlee.info/kbd/X11_mouse_button_numbering.html
 
+  (global-set-key (kbd "<mouse-3>") 'describe-char) ; right button
+
 ;; set the “forward button” (5th button) to close.
 (cond
  ((string-equal system-type "windows-nt") ; Windows
   (global-set-key (kbd "<mouse-5>") 'ergoemacs-close-current-buffer)
-  (global-set-key (kbd "<mouse-4>") 'describe-char)
+
+  (global-set-key (kbd "<C-wheel-up>") 'ergoemacs-previous-user-buffer) ; control wheel up
+  (global-set-key (kbd "<C-wheel-down>") 'ergoemacs-next-user-buffer) ; control wheel down
+
+  (global-set-key (kbd "<S-wheel-up>") 'ergoemacs-previous-emacs-buffer) ; shift + wheel up
+  (global-set-key (kbd "<S-wheel-down>") 'ergoemacs-next-emacs-buffer) ; shift + wheel down
+
+  (global-set-key (kbd "<C-S-wheel-up>") 'text-scale-increase) ; control+shift wheel up
+  (global-set-key (kbd "<C-S-wheel-down>") 'text-scale-decrease) ; control+shift wheel down
+
   )
+
  ((string-equal system-type "gnu/linux")
   (global-set-key (kbd "<mouse-9>") 'ergoemacs-close-current-buffer)
 
@@ -28,8 +40,7 @@
 
   (global-set-key (kbd "<C-S-mouse-4>") 'text-scale-increase) ; control+shift wheel up
   (global-set-key (kbd "<C-S-mouse-5>") 'text-scale-decrease) ; control+shift wheel down
-  (global-set-key (kbd "<mouse-3>") 'describe-char) ; right button
-
   )
+
  ((string-equal system-type "darwin") ; Mac
   (global-set-key (kbd "<mouse-5>") 'ergoemacs-close-current-buffer) ) )
