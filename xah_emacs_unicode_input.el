82ca55487978d8a9dede48ef721be3e36fda3631
Xah Lee <xah@xahlee.org>
2014-06-05 18:08:28 -0700

    complete redesign of keybinding that insert unicode char or brackets/quotes

--- xah_emacs_unicode_input.el
+++ xah_emacs_unicode_input.el
@@ -21,15 +21,13 @@
 (global-set-key (kbd "<menu> SPC") xah-unicode-keymap)
 
 (progn
-  (define-key key-translation-map (kbd "<menu> SPC SPC") (kbd "_")) ;low line (underscore)
-  (define-key key-translation-map (kbd "<menu> SPC RET") (kbd "-"))
+  (global-set-key (kbd "<menu> SPC <menu>") 'xah-insert-paren)
+  (define-key key-translation-map (kbd "<menu> SPC SPC") (kbd "_")) ; low line (underscore)
+  (global-set-key (kbd "<menu> SPC RET") 'xah-insert-unicode)
 
-  (define-key key-translation-map (kbd "<menu> SPC -") (kbd "—")) ; EM DASH
+  (define-key key-translation-map (kbd "<menu> SPC .") nil) 
 
-  (define-key key-translation-map (kbd "<menu> SPC . <down>") (kbd "⇓"))
-  (define-key key-translation-map (kbd "<menu> SPC . <left>") (kbd "⇐"))
-  (define-key key-translation-map (kbd "<menu> SPC . <right>") (kbd "⇒"))
-  (define-key key-translation-map (kbd "<menu> SPC . <up>") (kbd "⇑"))
+
   (define-key key-translation-map (kbd "<menu> SPC <down>") (kbd "↓"))
   (define-key key-translation-map (kbd "<menu> SPC <left>") (kbd "←"))
   (define-key key-translation-map (kbd "<menu> SPC <right>") (kbd "→"))
@@ -43,31 +41,34 @@
   (define-key key-translation-map (kbd "<menu> SPC 6") (kbd "ƒ"))
   (define-key key-translation-map (kbd "<menu> SPC 7") (kbd "＆"))
   (define-key key-translation-map (kbd "<menu> SPC 8") (kbd "•"))
+  (define-key key-translation-map (kbd "<menu> SPC 9") (kbd "—")) ; EM DASH
 
-  (global-set-key (kbd "<menu> SPC . b") 'xah-insert-white-lenticular-bracket〖〗)
-  (define-key key-translation-map (kbd "<menu> SPC c") (kbd "=")) ; equal
-
-  (global-set-key (kbd "<menu> SPC e") 'xah-insert-single-straight-quote)
-  (global-set-key (kbd "<menu> SPC u") 'xah-insert-double-straight-quote)
-  (global-set-key (kbd "<menu> SPC g") 'xah-insert-unicode)
-
-  (global-set-key (kbd "<menu> SPC h") 'xah-insert-brace)              ;{}
-  (global-set-key (kbd "<menu> SPC t") 'xah-insert-paren)              ;()
-
+  (global-set-key (kbd "<menu> SPC a") nil)
   (global-set-key (kbd "<menu> SPC b") 'xah-insert-black-lenticular-bracket【】)
-  (global-set-key (kbd "<menu> SPC . m") 'xah-insert-white-corner-bracket『』)
-  (global-set-key (kbd "<menu> SPC . w") 'xah-insert-double-angle-bracket《》)
-
+  (global-set-key (kbd "<menu> SPC c") 'xah-insert-ascii-single-quote)
   (global-set-key (kbd "<menu> SPC d") 'xah-insert-double-curly-quote“”)
-
-  (global-set-key (kbd "<menu> SPC i") 'xah-insert-single-curly-quote‘’)
+  (define-key key-translation-map (kbd "<menu> SPC e") (kbd "=")) ; equal
+  (global-set-key (kbd "<menu> SPC f") 'xah-insert-emacs-quote)
+  (global-set-key (kbd "<menu> SPC g") 'xah-insert-ascii-double-quote)
+  (global-set-key (kbd "<menu> SPC h") 'xah-insert-brace)              ;{}
+  (global-set-key (kbd "<menu> SPC i") 'xah-insert-curly-single-quote‘’)
+  (global-set-key (kbd "<menu> SPC j") nil)
+  (global-set-key (kbd "<menu> SPC k") nil)
   (define-key key-translation-map (kbd "<menu> SPC l") (kbd "…")) ; HORIZONTAL ELLIPSIS
   (global-set-key (kbd "<menu> SPC m") 'xah-insert-corner-bracket「」)
   (global-set-key (kbd "<menu> SPC n") 'xah-insert-bracket)            ;[]
-  (global-set-key (kbd "<menu> SPC p") 'xah-insert-double-angle-quote«»)
-  (define-key key-translation-map (kbd "<menu> SPC r") (kbd "+")) ; plus
+  (global-set-key (kbd "<menu> SPC o") nil)
+  (define-key key-translation-map (kbd "<menu> SPC p") (kbd "+")) ; plus
+  (global-set-key (kbd "<menu> SPC q") nil)
+  (global-set-key (kbd "<menu> SPC r") nil)
+  (global-set-key (kbd "<menu> SPC s") nil)
+  (global-set-key (kbd "<menu> SPC t") 'xah-insert-paren)
+  (define-key key-translation-map (kbd "<menu> SPC u") (kbd "-")) ; minus
+
+  (global-set-key (kbd "<menu> SPC v") 'xah-insert-double-angle-quote«»)
   (global-set-key (kbd "<menu> SPC w") 'xah-insert-angle-bracket〈〉)
   (global-set-key (kbd "<menu> SPC x") 'xah-insert-tortoise-shell-bracket〔〕)
   (global-set-key (kbd "<menu> SPC y") 'xah-insert-single-angle-quote‹›)
-  (global-set-key (kbd "<menu> SPC o") 'xah-insert-emacs-quote)
+  (global-set-key (kbd "<menu> SPC z") nil)
+
   )
