4e427c8d38935ead45a1a1d0c0e68fafd2d921f2
Xah Lee <xah@xahlee.org>
2015-01-17 18:32:01 -0800

    normal

--- xah_emacs_keybinding_mode_specific.el
+++ xah_emacs_keybinding_mode_specific.el
@@ -4,40 +4,38 @@
 (defun xah-html-mode-keys ()
   "Modify keymaps used by `html-mode'."
 
-  (define-key xhm-keymap (kbd "<delete>") xhm-single-keys-keymap)
+  ;; (define-key xhm-keymap (kbd "<delete>") xhm-single-keys-keymap)
 
-  (local-set-key (kbd "<backspace>") 'delete-backward-char)
+  (define-key xhm-single-keys-keymap (kbd "4") 'xahsite-update-article-timestamp)
+  (define-key xhm-single-keys-keymap (kbd "5") 'xhm-mark-unicode)
 
-  (local-set-key (kbd "<delete> 4") 'xahsite-update-article-timestamp)
-  (local-set-key (kbd "<delete> 5") 'xhm-mark-unicode)
+  (define-key xhm-single-keys-keymap (kbd "SPC b") 'xah-angle-brackets-to-html)
+  (define-key xhm-single-keys-keymap (kbd "SPC c") 'xwe-chinese-linkify)
+  (define-key xhm-single-keys-keymap (kbd "SPC d") 'xah-html-perldoc-ref-linkify)
+  (define-key xhm-single-keys-keymap (kbd "SPC e") 'xah-html-emacs-ref-linkify)
+  (define-key xhm-single-keys-keymap (kbd "SPC f") 'xah-html-full-size-img-linkify)
+  (define-key xhm-single-keys-keymap (kbd "SPC g") 'xah-clojure-word-ref-linkify)
+  (define-key xhm-single-keys-keymap (kbd "SPC j") 'xah-image-file-to-html-figure-tag)
+  (define-key xhm-single-keys-keymap (kbd "SPC p") 'xah-html-php-ref-linkify)
+  (define-key xhm-single-keys-keymap (kbd "SPC r") 'xah-add-to-related-links)
+  (define-key xhm-single-keys-keymap (kbd "SPC t") 'xwe-word-etymology-linkify)
+  (define-key xhm-single-keys-keymap (kbd "SPC z") 'xah-amazon-linkify)
 
-  (local-set-key (kbd "<delete> SPC b") 'xah-angle-brackets-to-html)
-  (local-set-key (kbd "<delete> SPC c") 'xwe-chinese-linkify)
-  (local-set-key (kbd "<delete> SPC d") 'xah-html-perldoc-ref-linkify)
-  (local-set-key (kbd "<delete> SPC e") 'xah-html-emacs-ref-linkify)
-  (local-set-key (kbd "<delete> SPC f") 'xah-html-full-size-img-linkify)
-  (local-set-key (kbd "<delete> SPC g") 'xah-clojure-word-ref-linkify)
-  (local-set-key (kbd "<delete> SPC j") 'xah-image-file-to-html-figure-tag)
-  (local-set-key (kbd "<delete> SPC p") 'xah-html-php-ref-linkify)
-  (local-set-key (kbd "<delete> SPC r") 'xah-add-to-related-links)
-  (local-set-key (kbd "<delete> SPC t") 'xwe-word-etymology-linkify)
-  (local-set-key (kbd "<delete> SPC z") 'xah-amazon-linkify)
-
-  (local-set-key (kbd "<delete> a") 'xwe-annotate)
-  (local-set-key (kbd "<delete> b") 'make-blogger-entry)
-  (local-set-key (kbd "<delete> c") 'xah-brackets-to-html)
-  (local-set-key (kbd "<delete> d") 'xah-html-insert-date-tag)
-  (local-set-key (kbd "<delete> e") 'xah-make-atom-entry)
-  (local-set-key (kbd "<delete> f") 'xah-copy-url-current-file)
-  (local-set-key (kbd "<delete> g") 'xah-browse-url-of-buffer)
-  (local-set-key (kbd "<delete> h") 'xah-all-linkify)
-  (local-set-key (kbd "<delete> i") 'xah-html-image-linkify)
-  (local-set-key (kbd "<delete> n") 'xah-ref-span-tag)
-  (local-set-key (kbd "<delete> z a") 'xah-html-insert-keywords-tag)
-  (local-set-key (kbd "<delete> z b") 'xah-html-insert-lyrics-header)
-  (local-set-key (kbd "<delete> z c") 'xah-html-insert-lyrics-table)
-  (local-set-key (kbd "<delete> z d") 'xah-html-insert-screen-filler)
-  (local-set-key (kbd "<delete> z f") 'xah-html-insert-midi)
+  (define-key xhm-single-keys-keymap (kbd "a") 'xwe-annotate)
+  (define-key xhm-single-keys-keymap (kbd "b") 'make-blogger-entry)
+  (define-key xhm-single-keys-keymap (kbd "c") 'xah-brackets-to-html)
+  (define-key xhm-single-keys-keymap (kbd "d") 'xah-html-insert-date-tag)
+  (define-key xhm-single-keys-keymap (kbd "e") 'xah-make-atom-entry)
+  (define-key xhm-single-keys-keymap (kbd "f") 'xah-copy-url-current-file)
+  (define-key xhm-single-keys-keymap (kbd "g") 'xah-browse-url-of-buffer)
+  (define-key xhm-single-keys-keymap (kbd "h") 'xah-all-linkify)
+  (define-key xhm-single-keys-keymap (kbd "i") 'xah-html-image-linkify)
+  (define-key xhm-single-keys-keymap (kbd "n") 'xah-ref-span-tag)
+  (define-key xhm-single-keys-keymap (kbd "z a") 'xah-html-insert-keywords-tag)
+  (define-key xhm-single-keys-keymap (kbd "z b") 'xah-html-insert-lyrics-header)
+  (define-key xhm-single-keys-keymap (kbd "z c") 'xah-html-insert-lyrics-table)
+  (define-key xhm-single-keys-keymap (kbd "z d") 'xah-html-insert-screen-filler)
+  (define-key xhm-single-keys-keymap (kbd "z f") 'xah-html-insert-midi)
 
 )
 
@@ -47,7 +45,7 @@
 
 (defun xah-css-mode-setup ()
   "Modify keymaps used by `xah-css-mode'."
-  (local-set-key (kbd "<delete> s") 'xah-sync-css))
+  (local-set-key (kbd "<menu> e s") 'xah-sync-css))
 
 (add-hook 'xah-css-mode-hook 'xah-css-mode-setup)
 
@@ -64,7 +62,7 @@ For `rcirc-mode-hook'."
 (defun xah-Info-mode-keys ()
   "Modify keybindings for `Info-mode'.
 For `Info-mode-hook'."
-  (local-set-key (kbd "<delete> g") 'xah-view-emacs-manual-in-browser)
+  (local-set-key (kbd "<menu> e g") 'xah-view-emacs-manual-in-browser)
   (local-set-key (kbd "<mouse-8>") 'Info-history-back)
   )
 (add-hook 'Info-mode-hook 'xah-Info-mode-keys)
