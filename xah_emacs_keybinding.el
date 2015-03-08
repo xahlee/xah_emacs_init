286a8899c04af273ed73fedbe6ac01203404612c
Xah Lee <xah@xahlee.org>
2015-03-08 16:29:36 -0700

    normal

--- xah_emacs_keybinding.el
+++ xah_emacs_keybinding.el
@@ -1,3 +1,6 @@
 ;; -*- coding: utf-8 -*-
 
-;; if you are looking for xah-fly-keys, see http://ergoemacs.org/misc/ergoemacs_vi_mode.html
+;; xah-fly-keys move to http://ergoemacs.org/misc/ergoemacs_vi_mode.html
+
+(global-set-key (kbd "<C-tab>") 'xah-next-user-buffer)
+(global-set-key (kbd "<C-S-iso-lefttab>") 'xah-previous-user-buffer)
