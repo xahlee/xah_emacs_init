21e16370a6a9f3cf815dcb9e7c91ca9b04d16f45
Xah Lee <xah@xahlee.org>
2020-08-15 20:58:18 -0700

    no fear of mouse

--- xah_emacs_mouse_setup.el
+++ xah_emacs_mouse_setup.el
@@ -10,30 +10,12 @@
 ;; mouse
 
 ;; (setq mouse-yank-at-point t)
+
+;; 2020-08-15 make mouse wheel scroll just 1 line. this is great for logitech's spin wheel
+(when (>= emacs-major-version 27)
+  (customize-set-variable
+   'mouse-wheel-scroll-amount
+   '(1)))
+
+;; 2020-08-15 this is great for logitech's spin wheel
 (setq mouse-wheel-progressive-speed nil)
-
-;; set the “forward button” (5th button) to close.
-(cond
- ((string-equal system-type "gnu/linux")
-  (progn
-
-    (global-set-key (kbd "<S-mouse-4>") 'xah-previous-user-buffer)
-    (global-set-key (kbd "<S-mouse-5>") 'xah-next-user-buffer)
-
-    (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
-    (global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
-
-    (global-set-key (kbd "<mouse-6>") 'xah-beginning-of-line-or-block)
-    (global-set-key (kbd "<mouse-7>") 'xah-end-of-line-or-block)))
- ((string-equal system-type "darwin") ; Mac
-  (progn
-
-    (global-set-key (kbd "<S-wheel-left>") 'xah-previous-user-buffer)
-    (global-set-key (kbd "<S-wheel-right>") 'xah-next-user-buffer)
-
-    (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
-    (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)))
-
- ((string-equal system-type "windows-nt") ; Windows
-  (progn
-    nil)))
