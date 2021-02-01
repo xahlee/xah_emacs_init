0969bbf26562a4060ca9ca32d4e8725e1207a838
Xah Lee <xah@xahlee.org>
2021-01-31 21:36:15 -0800

    get children

--- xah_emacs_misc.el
+++ xah_emacs_misc.el
@@ -362,11 +362,11 @@ Version 2017-02-02"
 (setq xah-interactive-abbrev-alist
       '(
         ("PowerShell diff" . "diff (cat f1) (cat f2) ")
-        ("PowerShell emacs backup~" . "Get-ChildItem . -name -Recurse -Include *~")
-        ("PowerShell .DS_Store" . "Get-ChildItem . -name -Recurse -Include '.DS_Store'")
-        ("PowerShell #emacs_auto_save#" . "Get-ChildItem . -name -Recurse -Include '#*#'")
-        ("PowerShell htaccess" . "Get-ChildItem . -Recurse -name -Include '.htaccess'")
-        ("PowerShell xx temp file" . "Get-ChildItem . -name -Recurse -Include 'xx*'")
+        ("PowerShell emacs backup~" . "Get-ChildItem -name -Recurse -Include *~")
+        ("PowerShell .DS_Store" . "Get-ChildItem -name -Recurse -Include '.DS_Store'")
+        ("PowerShell #emacs_auto_save#" . "Get-ChildItem -name -Recurse -Include '#*#'")
+        ("PowerShell htaccess" . "Get-ChildItem -Recurse -name -Include '.htaccess'")
+        ("PowerShell xx temp file" . "Get-ChildItem -name -Recurse -Include 'xx*'")
         ("image convert" . "convert -quality 85% ")
         ("image scale" . "convert -scale 50% -quality 85% ")
         ("image 256" . "convert +dither -colors 256 ")
